0cf4aa4446d9e36d0af8959028c37aaab3795309
chitranshoo <chitranshoo10081998@gmail.com>
2022-06-03 23:45:33 +0530

    data_bindind

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
 
-export class ServerComponent {
+export class ServerComponent implements OnInit{
+  serverId = 12;
+  serverStatus = 'offline';
+  allowNewServer = false;
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+
+  }
 }
